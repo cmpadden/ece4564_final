b63eea8ea58dec245a221f03575bc352dced01b6
Chris Cronin <chriscro@vt.edu>
2014-12-15 10:27:36 -0500

    Added CLA parsing for pika connection info and fixed exception handling bug

--- monitor.py
+++ monitor.py
@@ -1,27 +1,39 @@
 #!/usr/bin/env python
 
 import pika
+import logging
 import sys
 import json
 import time
+import argparse
 from test_service import TestService
 from gmail import GmailService
 from weather import WeatherService
 
+logging.getLogger('pika').setLevel(logging.ERROR)
+
 # Instantiate the services
 s1 = TestService()
-#s2 = GmailService()
-s3 = WeatherService()
-services = [s1, s3];
+s2 = GmailService()
+#s3 = WeatherService()
+services = [s1, s2];
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(description='Stat Cleint')
+parser.add_argument('-b', metavar='broker-address', dest='ip', required=True)
+parser.add_argument('-p', metavar='virtual-host', dest='virtualhost', default='/')
+parser.add_argument('-c', metavar='username:password', dest='credentials', default='guest:guest')
+cla = parser.parse_args()
 
 try:
 	#connect to message broker
+	credentials = cla.credentials.split(':')
 	msg_broker = pika.BlockingConnection(
-		pika.ConnectionParameters(host="netapps.ece.vt.edu",
-								  virtual_host="sandbox",
-								  credentials=pika.PlainCredentials("ECE4564-Fall2014",
-								  									 "13ac0N!",
-								  									 True)))
+	pika.ConnectionParameters(host=cla.ip,
+							  virtual_host=cla.virtualhost,
+							  credentials=pika.PlainCredentials(credentials[0],
+							 								    credentials[1],
+							  								    True)))
 	#setup Exchange
 	channel = msg_broker.channel()
 	channel.exchange_declare(exchange="services", type="direct")
@@ -33,10 +45,10 @@ try:
 			
 			if service.doUpdate():								# If the service should be updated
 				try:
-					if hasattr(service, 'comment'):
-						service.comment()
 					data[service.getName()] = service.getPriority()	# Add the name and priority to the dict
 					dataString = json.dumps(data)					# Dump the data to json
+					if hasattr(service, 'comment'):
+						service.comment()
 				except:
 					print("Unable to update " + service.getName() + ", skipping...")
 		if data:												# If there is updated info to send
@@ -50,5 +62,10 @@ except KeyboardInterrupt:
 		if hasattr(service, 'doCleanup'):
 			service.doCleanup()
 
-# close the connection
-msg_broker.close()
+except Exception, ee:
+	print("Error: {0}".format(ee))
+
+finally:
+	# close the connection
+	msg_broker.close()	
+
